8b178d9b8298f073e415c700ccb09dbee9befec6
Honza Dvorsky <honza@swiftkey.com>
2015-03-22 13:05:09 +0000

    making run.sh all you need to ... well, *run*.

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Install packages
+npm install
+
+# Install typings
+./node_modules/.bin/tsd reinstall
+
 # Transpile
-node ./node_modules/.bin/tsc --sourcemap --module commonjs ./bin/www.ts
+./node_modules/.bin/tsc --sourcemap --module commonjs ./bin/www.ts
 
 # Run
 DEBUG=Express-4x-Typescript-Sample:* ./bin/www
